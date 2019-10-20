d19bec44dd010e57a5c44e9d89a089e72a5a2e62
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-10-20 02:54:22 +0200

    add gf/ directory for prior data

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # tree structure
-mkdir -p doc hg19 huvec imr90
+mkdir -p gf doc hg19 huvec imr90
 
 # documentation
 wget https://www.encodeproject.org/documents/a4a6caad-61ab-4d35-820a-409cadce1121/@@download/attachment/RSEM_quantifications_specifications.txt -O doc/rnaseq.tsv.rsem.spec.txt

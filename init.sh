83665b0c461036a53e241f72ac177cd8375f578b
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-10-21 20:59:09 +0200

    init: don't change directories and split repseq into simple pro-A/pro-B subsets

--- init.sh
+++ init.sh
@@ -9,22 +9,35 @@ wget -nc https://www.encodeproject.org/documents/a4a6caad-61ab-4d35-820a-409cadc
 if [[ ! -e hg19/README ]]; then
 	w3m -dump http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/ >hg19/README
 fi
-cd hg19
 # hg19 repeatmasker output
-wget -nc 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.out.gz'
+wget -nc -O hg19/hg19.fa.out.gz 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.out.gz'
 # hg19 chromosome info (for windowing)
-mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e "select chrom, size from hg19.chromInfo" >hg19/hg19.txt
-# hg19 promoters (fasta only, not really useful?)
-#wget -nc 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/upstream*gz'
+if [[ ! -f hg19/hg19.txt ]]; then
+	mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e "select chrom, size from hg19.chromInfo" >hg19/hg19.txt
+fi
 # hg19 ensembl regulation GFF, convert to BED and extract promoters and enhancers only
-if [[ ! -f hg19.promenh.20180925.bed ]]; then
+if [[ ! -f hg19/hg19.promenh.20180925.bed ]]; then
 	wget -O - ftp://ftp.ensembl.org/pub/grch37/release-98/regulation/homo_sapiens/homo_sapiens.GRCh37.Regulatory_Build.regulatory_features.20180925.gff.gz |\
 		gunzip -c |\
 		gff2bed |\
 		sed -n '/promoter:\|enhancer:/s/^/chr/p' \
-		>hg19.promenh.20180925.bed
+		>hg19/hg19.promenh.20180925.bed
 fi
-cd ..
 
 # download files specified in csv's, convert excel stuff
 Rscript init.R
+
+# extract repseq subclasses
+awk '
+NR > 1{
+	if($4 > 0.04)
+		print $0
+}
+' gf/huvec.repseq.tsv > gf/huvec.proa.tsv
+
+awk '
+NR > 1{
+	if($4 < -0.01)
+		print $0
+}
+' gf/huvec.repseq.tsv > gf/huvec.prob.tsv

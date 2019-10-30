683f7eba29e0c11992b0985b35f1812435348580
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-10-30 20:37:15 +0100

    init: only download inputs, don't uncompress or transform them here

--- init.sh
+++ init.sh
@@ -17,29 +17,25 @@ getchr(){
 }
 
 # tree structure
-mkdir -p gf hg19 huvec imr90 cnt plot
+mkdir -p hg19 huvec imr90 cnt plot
+
+if [[ ! -f gf/Kassiotis-List.ORI.RepSeq.CorrB-Fourel.11july.xlsx || ! -f gf/Table-AouBouAlways.xlsx ]]; then
+	echo 'missing input files in gf/ subdirectory!'
+	exit 1
+fi
 
 # hg19
 if [[ ! -e hg19/README ]]; then
 	w3m -dump http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/ >hg19/README
 fi
 # hg19 repeatmasker output
-if [[ ! -f hg19/hg19.fa.out ]]; then
-	wget -nc -O hg19/hg19.fa.out.gz 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.out.gz'
-	gunzip hg19/hg19.fa.out.gz
-fi
+wget -nc -P hg19/ 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.out.gz'
 # hg19 known genes (ncbi refseq)
-wget -nc -O hg19/ncbiRefSeqCurated.txt.gz 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/ncbiRefSeq.txt.gz'
+wget -nc -P hg19/ 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/ncbiRefSeq.txt.gz'
 # hg19 ensembl regulation GFF, convert to BED and extract promoters and enhancers only
-if [[ ! -f hg19/hg19.promenh.20180925.bed.gz ]]; then
-	wget -O - ftp://ftp.ensembl.org/pub/grch37/release-98/regulation/homo_sapiens/homo_sapiens.GRCh37.Regulatory_Build.regulatory_features.20180925.gff.gz |\
-		gunzip -c |\
-		gff2bed |\
-		sed -n '/promoter:\|enhancer:/s/^/chr/p' |\
-		gzip -c >hg19/hg19.promenh.20180925.bed.gz
-fi
+wget -nc -P hg19/ ftp://ftp.ensembl.org/pub/grch37/release-98/regulation/homo_sapiens/homo_sapiens.GRCh37.Regulatory_Build.regulatory_features.20180925.gff.gz
 # hg19 %gc track
-wget -nc 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.gc5Base.wigVarStep.gz'
+wget -nc -P hg19/ 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.gc5Base.wigVarStep.gz'
 # hg19 chromHMM tracks
 wget -nc -P hg19/ 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeBroadHmm/*.gz'
 

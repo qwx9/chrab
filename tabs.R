38927329a54d21c1cef937ea4a56003681e9ae60
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-01 23:16:12 +0100

    tabs: generalize pulling count data and generate table with all counts

--- tabs.R
+++ tabs.R
@@ -1,17 +1,29 @@
 require(dplyr)
 
+addcol <- function(chr, f){
+	read.table(f) %>%
+		filter(V1 %in% unique(chr)) %>%
+		pull(V4)
+}
+
 # generate a/b classes
-ab <- read.table("prep/ab.bed", header=TRUE)
-ab$ngene <- read.table("cnt/hg19w.hg19.refseq.bed.gz") %>%
-	filter(V1 %in% unique(ab$chr)) %>%
-	pull(V4)
-ab$nact <- read.table("cnt/hg19w.huvec.proa.genes.bed.gz") %>%
-	filter(V1 %in% unique(ab$chr)) %>%
-	pull(V4)
-ab <- ab %>%
+ab <- read.table("prep/ab.bed", header=TRUE) %>%
+	mutate(ngene=addcol(chr, "cnt/hg19w.hg19.refseq.bed.gz")) %>%
+	mutate(nact=addcol(chr, "cnt/hg19w.huvec.proa.genes.bed.gz")) %>%
 	mutate(class1=ifelse(HUVEC < 0, "B", "A"),
 		class2=ifelse(ngene >= 4, "highgenedensity", ifelse(ngene > 0, "normalgenedensity", "nogene")),
 		class3=ifelse(nact > 0, "hasactive", "noactive"),
 		class=paste(class1, class2, AorBvec, class3, sep="_")) %>%
 	select(-class1, -class2, -class3)
-write.table(ab, "tabs/class.txt", sep="\t", row.names=FALSE, quote=FALSE)
+write.table(ab, "tabs/class.tsv", sep="\t", row.names=FALSE, quote=FALSE)
+
+for(i in list.files("cnt")){
+	if(i %in% c("hg19w.hg19.refseq.bed.gz", "hg19w.huvec.proa.genes.bed.gz"))
+		next
+	s <- gsub("\\.bed\\.gz$", "", gsub("^cnt/[^\\.]+\\.", "", i))
+	ab <- ab %>%
+		mutate(!!s:=addcol(chr, paste0("cnt/", i)))
+}
+gfd <- gzfile("tabs/aball.tsv.gz", "w")
+write.table(ab, gfd, sep="\t", row.names=FALSE, quote=FALSE)
+close(gfd)

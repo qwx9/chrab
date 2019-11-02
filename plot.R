59c5d8398163e57e6cff5d7978f097df6b234602
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-02 14:57:14 +0100

    plot: fix access checks and add code to generate all scatter plots

--- plot.R
+++ plot.R
@@ -3,21 +3,29 @@ require(ggplot2)
 
 ggviolin <- function(ab, var){
 	f <- paste0("plot/violin.", var, ".pdf")
-	if(file.access(f, 4) == 0)
-		return
-	pdf(f, width=12.1, height=9.7)
-	g <- ggplot(ab, aes(class, !!sym(var))) +
-		geom_violin(na.rm=TRUE) +
-		theme(axis.text.x=element_text(angle=90,vjust=-0.1))
-	print(g)
-	dev.off()
+	if(file.access(f, 4) != 0){
+		pdf(f, width=12.1, height=9.7)
+		g <- ggplot(ab, aes(class, !!sym(var))) +
+			geom_violin(na.rm=TRUE) +
+			theme(axis.text.x=element_text(angle=90,vjust=-0.1))
+		print(g)
+		dev.off()
+	}
+}
+
+ggscatter <- function(ab, var, pc1){
+	f <- paste0("plot/scatter.", var, ".pdf")
+	if(file.access(f, 4) != 0){
+		pdf(f, width=12.1, height=9.7)
+		g <- ggplot(ab, aes(!!sym(var), !!sym(pc1))) +
+			geom_hex(na.rm=TRUE)
+		print(g)
+		dev.off()
+	}
 }
 
 ab <- read.table("tabs/aball.tsv.gz", header=TRUE)
 ggviolin(ab, "ngene")
 ggviolin(ab, "nact")
-
-#ggplot(ab, aes(ngene, HUVEC)) + geom_hex()
-#ggplot(ab, aes(nact, HUVEC)) + geom_hex()
-#ab$prma <- read.table("cnt/hg19w.cnt.huvec.proa.prm.bed")$V4
-#ggplot(ab, aes(prma, HUVEC)) + geom_hex()
+for(i in colnames(ab)[-c(1:7, 9)])
+	ggscatter(ab, i, "HUVEC")

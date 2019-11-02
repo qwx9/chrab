8ac1f84947d8ea57c071db4848c7935ac4111f5a
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-02 12:31:47 +0100

    plot: generalize violin plot function and load complete table

--- plot.R
+++ plot.R
@@ -1,21 +1,21 @@
 require(dplyr)
 require(ggplot2)
 
-ab <- read.table("tabs/class.txt", header=TRUE)
+ggviolin <- function(ab, var){
+	f <- paste0("plot/violin.", var, ".pdf")
+	if(file.access(f, 4) == 0)
+		return
+	pdf(f, width=12.1, height=9.7)
+	g <- ggplot(ab, aes(class, !!sym(var))) +
+		geom_violin(na.rm=TRUE) +
+		theme(axis.text.x=element_text(angle=90,vjust=-0.1))
+	print(g)
+	dev.off()
+}
 
-# per class violin plots
-pdf("plot/violin.ngene.pdf", width=12.1, height=9.7)
-	g <- ggplot(ab, aes(class, ngene)) +
-		geom_violin(na.rm=TRUE) +
-		theme(axis.text.x=element_text(angle=90,vjust=-0.1))
-	print(g)
-dev.off()
-pdf("plot/violin.nact.pdf", width=12.1, height=9.7)
-	g <- ggplot(ab, aes(class, ngene)) +
-		geom_violin(na.rm=TRUE) +
-		theme(axis.text.x=element_text(angle=90,vjust=-0.1))
-	print(g)
-dev.off()
+ab <- read.table("tabs/aball.tsv.gz", header=TRUE)
+ggviolin(ab, "ngene")
+ggviolin(ab, "nact")
 
 #ggplot(ab, aes(ngene, HUVEC)) + geom_hex()
 #ggplot(ab, aes(nact, HUVEC)) + geom_hex()

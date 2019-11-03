2dcd193e0e027bdcbe519e2669a8944869a1f3ff
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-03 20:01:04 +0100

    install: don't reinstall R packages every time

--- install.R
+++ install.R
@@ -1,1 +1,4 @@
-install.packages(c("readxl", "dplyr", "ggplot2", "hexbin", "doParallel", "dbplyr", "RMySQL"))
+l <- c("readxl", "dplyr", "ggplot2", "hexbin", "doParallel", "dbplyr", "RMySQL")
+l <- l[! l %in% installed.packages()[,"Package"]]
+if(length(l) > 0)
+	install.packages(l)

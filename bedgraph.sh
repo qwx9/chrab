60285b831624493f246e835fd1e74a5ff28bf0c9
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-03 12:13:54 +0100

    bedgraph: remove more bashisms

--- bedgraph.sh
+++ bedgraph.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
+f=/tmp/bedgraph.tmp
 mkdir -p igv
 cd cnt
 for i in *.bed.gz; do
-	cat <(echo 'track type=bedGraph visibility=full color=200,100,0 altColor=0,100,200 priority=20 height=200 name='$i) <(gunzip -c $i) |\
-		gzip -c >../igv/`echo $i | sed 's/\.bed\./.bedgraph./'`
+	echo 'track type=bedGraph visibility=full color=200,100,0 altColor=0,100,200 priority=20 height=200 name='$i >$f
+	gunzip -c $i >>$f
+	gzip -c <$f >../igv/`echo $i | sed 's/\.bed\./.bedgraph./'`
 done
+rm -f $f

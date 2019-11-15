74c1690c1aa2b2384079086ead17da8a799d2875
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-15 21:01:03 +0100

    install: add ggridges and gridExtra dependencies

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-l <- c("readxl", "dplyr", "ggplot2", "hexbin", "doParallel", "dbplyr", "RMySQL")
+l <- c("readxl", "dplyr", "ggplot2", "hexbin", "doParallel", "dbplyr", "RMySQL", "ggridges", "gridExtra")
 l <- l[! l %in% installed.packages()[,"Package"]]
 if(length(l) > 0)
 	install.packages(l)

550c318df2645b053e37ec924794647f667e13e5
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-15 21:05:10 +0100

    plot: add ridgeline plots and combine plots for each element into single file
    - filter NA_* subclasses from table for violin and ridgeline plots
    - do file access checks prior to parallel processing

--- plot.R
+++ plot.R
@@ -1,42 +1,58 @@
 require(dplyr)
 require(ggplot2)
+require(ggridges)
+require(gridExtra)
 require(doParallel)
 
 ggviolin <- function(ab, var, class){
-	f <- paste0("plot/violin.", var, sub("F", ".noflank", substring(class, 6)), ".pdf")
-	if(file.access(f, 4) != 0){
-		pdf(f, width=12.1, height=9.7)
-		g <- ggplot(ab, aes(!!sym(class), !!sym(var))) +
-			geom_violin(na.rm=TRUE) +
-			theme(axis.text.x=element_text(angle=90,vjust=-0.1))
-		print(g)
-		dev.off()
-	}
+	ggplot(ab, aes(!!sym(class), !!sym(var))) +
+		geom_violin(na.rm=TRUE) +
+		theme(axis.text.x=element_text(angle=90,vjust=-0.1))
+}
+
+ggridge <- function(ab, var, class){
+	ggplot(ab, aes(!!sym(var), !!sym(class), fill=!!sym(class))) +
+		scale_fill_discrete(guide=FALSE) +
+		geom_density_ridges2(na.rm=TRUE)
 }
 
 ggscatter <- function(ab, var, pc1){
-	f <- paste0("plot/scatter.", tolower(pc1), ".", var, ".pdf")
-	if(file.access(f, 4) != 0){
-		pdf(f, width=12.1, height=9.7)
-		g <- ggplot(ab, aes(!!sym(var), !!sym(pc1))) +
-			geom_hex(na.rm=TRUE)
-		print(g)
-		dev.off()
-	}
+	ggplot(ab, aes(!!sym(var), !!sym(pc1))) +
+		geom_hex(na.rm=TRUE)
 }
 
 ab <- read.table("tabs/aball.tsv.gz", header=TRUE) %>%
 	select(-chr, -start, -end, -AorBvec)
 l <- colnames(ab)
 l <- l[grep("^(hg19w\\.NA_|hg19w\\.[AB]_|class|HUVEC|IMR90)", l, invert=TRUE)]
-ab <- lapply(l, function(x) select(ab, class, classF, HUVEC, HUVECnoflank, !!sym(x)))
+f <- paste0("plot/", l, ".pdf")
+i <- which(file.access(f, 4) != 0)
+if(length(i) == 0)
+	return
+l <- l[i]
+f <- f[i]
+abf <- lapply(l, function(x){
+	ab %>%
+		select(class, HUVEC, !!sym(x)) %>%
+		filter(!grepl("^NA_", class))
+})
+abnf <- lapply(l, function(x){
+	ab %>%
+		select(classF, HUVECnoflank, !!sym(x)) %>%
+		filter(!grepl("^NA_", classF))
+})
 nc <- detectCores()
 cl <- makeCluster(nc)
 registerDoParallel(cl)
-l <- foreach(i=l, ab=ab, .inorder=FALSE, .packages="ggplot2") %dopar% {
-	ggviolin(ab, i, "class")
-	ggviolin(ab, i, "classF")
-	ggscatter(ab, i, "HUVEC")
-	ggscatter(ab, i, "HUVECnoflank")
+l <- foreach(i=l, f=f, abf=abf, abnf=abnf, .inorder=FALSE, .packages=c("ggplot2", "ggridges", "gridExtra")) %dopar% {
+	g1 <- ggviolin(abf, i, "class")
+	g2 <- ggviolin(abnf, i, "classF")
+	g3 <- ggridge(abf, i, "class")
+	g4 <- ggridge(abnf, i, "classF")
+	g5 <- ggscatter(abf, i, "HUVEC")
+	g6 <- ggscatter(abnf, i, "HUVECnoflank")
+	pdf(f, width=24, height=20)
+	print(grid.arrange(g1, g2, g3, g4, g5, g6))
+	dev.off()
 }
 stopCluster(cl)

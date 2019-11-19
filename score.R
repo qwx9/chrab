60baf97b032145e51266eed355f14ce68d1abc1b
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-11-19 18:56:37 +0100

    score: full rewrite
    - list models to generate instead of doing everything
    - segregate model output in its own directory
    - generate bedgraph with full prediction based on coefficient values with flanking regions

--- score.R
+++ score.R
@@ -1,27 +1,36 @@
 library(dplyr)
 
-model <- function(ab, pred, expl){
-	fx <- quote(paste0(pred, "~", paste0(expl, collapse="+")))
+model <- function(ab, dir, expl){
+	dir.create(dir)
+	fx <- quote(paste0("HUVECnoflank ~", paste0(expl, collapse="+")))
 	m <- lm(eval(fx), ab)
-	f <- paste0("score/", pred, ".", paste0(expl, collapse="."))
-	f <- gsub("(huvec|chromhmm\\.[0-9]|repseq)\\.", "", f)
-	if(file.access(paste0(f, ".txt")) == 0)
-		return(NULL)
-	sink(paste0(f, ".txt"))
+	sink(paste0(dir, "summary.txt"))
 	print(summary(m))
 	sink()
-	pdf(paste0(f, ".pdf"), width=12.1, height=9.7)
+	pdf(paste0(dir, "plots.pdf"), width=12.1, height=9.7)
 	layout(matrix(c(1,2,3,4),2,2))
 	plot(m)
 	dev.off()
-	gfd <- gzfile(paste0(f, ".bedgraph.gz"), "wb")
+	gfd <- gzfile(paste0(dir, "noflank.bedgraph.gz"), "wb")
 	cat(paste0("track type=bedGraph visibility=full",
-		"color=200,100,0 altColor=0,100,200 priority=20 height=200 name=", f, "\n"),
+		"color=200,100,0 altColor=0,100,200 priority=20 height=200 name=", dir, "noflank\n"),
 		file=gfd)
 	ab %>%
-		filter(!is.na(!!sym(pred))) %>%
+		filter(!is.na(HUVECnoflank)) %>%
 		select(chr, start, end) %>%
 		mutate(v=m$fitted.values) %>%
+		write.table(gfd,
+			col.names=FALSE, row.names=FALSE, quote=FALSE, sep="\t", append=TRUE)
+	close(gfd)
+	gfd <- gzfile(paste0(dir, "full.bedgraph.gz"), "wb")
+	cat(paste0("track type=bedGraph visibility=full",
+		"color=200,100,0 altColor=0,100,200 priority=20 height=200 name=", dir, "full\n"),
+		file=gfd)
+	e <- sapply(seq_along(expl), function(i) ab[,expl[i]] * m$coefficients[1+i])
+	ab %>%
+		mutate(v=apply(e, 1, function(x) m$coefficients[1] + sum(x))) %>%
+		filter(!is.na(HUVEC)) %>%
+		select(chr, start, end, v) %>%
 		write.table(gfd,
 			col.names=FALSE, row.names=FALSE, quote=FALSE, sep="\t", append=TRUE)
 	close(gfd)
@@ -29,47 +38,65 @@ model <- function(ab, pred, expl){
 
 ab <- read.table("tabs/counts.tsv.gz", header=TRUE)
 
-proa <- list(
-	list(
-		"huvec.chromhmm.1.active_promoter",
-		"huvec.chromhmm.2.weak_promoter",
-		"huvec.chromhmm.3.poised_promoter",
-		"huvec.chromhmm.4.strong_enhancer",
-		"huvec.chromhmm.5.strong_enhancer",
-		"huvec.chromhmm.6.weak_enhancer",
-		"huvec.chromhmm.7.weak_enhancer",
-		"huvec.silencer"
-	),
-	list(
-		"huvec.h3k27ac",
-		"huvec.h3k4me3",
-		"huvec.dhs.rep1"
-	)
+chromcounts <- list(
+	"huvec.chromhmm.1.active_promoter",
+	"huvec.chromhmm.2.weak_promoter",
+	"huvec.chromhmm.4.strong_enhancer",
+	"huvec.chromhmm.5.strong_enhancer",
+	"huvec.chromhmm.6.weak_enhancer",
+	"huvec.chromhmm.7.weak_enhancer",
+	"huvec.chromhmm.3.poised_promoter",
+	"huvec.silencer"
 )
-prox <- list(
-	"hg19.gc",
+chromfeeling <- list(
+	"huvec.h3k27ac",
+	"huvec.h3k4me3",
+	"huvec.dhs.rep1"
+)
+additional <- list(
 	"huvec.groseq.score",
 	"huvec.repseq.proa"
 )
-prob <- list(
-	"huvec.repseq.ltrline",
-	"huvec.repseq.ltr",
-	"huvec.repseq.l1",
-	"huvec.repseq.l1.long",
+brepseq <- list(
 	"huvec.repseq.prob",
 	"huvec.repseq.prob.ltrline",
 	"huvec.repseq.prob.ltr",
-	"huvec.repseq.prob.line",
-	"huvec.repseq.prob.sr.lc.sat",
-	"huvec.repseq.psil.anyb",
-	"huvec.repseq.psil.prob",
-	"huvec.repseq.psil.subb"
+	list(
+		"huvec.repseq.psil.prob",
+		"huvec.repseq.prob.ltrline"
+	)
 )
-l <- lapply(proa, function(x) lapply(prob, function(y){
-	model(ab, "HUVEC", c(x, y))
-	model(ab, "HUVECnoflank", c(x, y))
-	lapply(prox, function(z){
-		model(ab, "HUVEC", c(x, y, z))
-		model(ab, "HUVECnoflank", c(x, y, z))
-	})
-}))
+
+l <- list(
+	chromcounts,
+	c(chromcounts, brepseq[[1]]),
+	c(chromcounts, brepseq[[2]]),
+	c(chromcounts, brepseq[[3]]),
+	c(chromcounts, brepseq[[4]]),
+	c(chromcounts, brepseq[[1]], additional[[1]]),
+	c(chromcounts, brepseq[[2]], additional[[1]]),
+	c(chromcounts, brepseq[[3]], additional[[1]]),
+	c(chromcounts, brepseq[[4]], additional[[1]]),
+	c(chromcounts, brepseq[[1]], additional[[2]]),
+	c(chromcounts, brepseq[[2]], additional[[2]]),
+	c(chromcounts, brepseq[[3]], additional[[2]]),
+	c(chromcounts, brepseq[[4]], additional[[2]]),
+	chromfeeling,
+	c(chromfeeling, brepseq[[1]]),
+	c(chromfeeling, brepseq[[2]]),
+	c(chromfeeling, brepseq[[3]]),
+	c(chromfeeling, brepseq[[4]]),
+	c(chromfeeling, brepseq[[1]], additional[[1]]),
+	c(chromfeeling, brepseq[[2]], additional[[1]]),
+	c(chromfeeling, brepseq[[3]], additional[[1]]),
+	c(chromfeeling, brepseq[[4]], additional[[1]]),
+	c(chromfeeling, brepseq[[1]], additional[[2]]),
+	c(chromfeeling, brepseq[[2]], additional[[2]]),
+	c(chromfeeling, brepseq[[3]], additional[[2]]),
+	c(chromfeeling, brepseq[[4]], additional[[2]])
+)
+l <- lapply(l, unlist)
+l <- l[which(sapply(seq_along(l), function(i) file.access(paste0("score/m", i)) != 0))]
+l <- lapply(seq_along(l), function(i){
+	model(ab, paste0("score/m", i, "/"), unlist(l[[i]]))
+})

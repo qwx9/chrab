13bb8b11a972d071d4267d443b604a9ddac7363a
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2019-12-23 20:27:15 +0100

    session: simplify script, make more readable

--- session.sh
+++ session.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # export R sessionInfo with all used packages for README
-grep 'library(' *.R | sed 's/^[^:]*://' | sort | uniq >/tmp/session.R
+grep -h 'library(' *.R |\
+	sort |\
+	uniq >/tmp/session.R
 echo 'sessionInfo()' >>/tmp/session.R
 Rscript /tmp/session.R 2>/dev/null
 rm /tmp/session.R

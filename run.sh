206d9c65eac21140860ac9b3b50f804665fc704f
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2020-01-03 03:03:16 +0100

    run: silence stdout output and print some progress reports

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -e
 # rerun entire pipeline from scratch
+exec >/dev/null
 ./clean.sh
 ./install.sh
+echo '-> downloading input files' >&2
 Rscript init.R
+echo '-> preparing input files' >&2
 Rscript prep.R
+echo '-> counting elements' >&2
 ./count.sh
+echo '-> generating count tables' >&2
 Rscript tabs.R
+echo '-> generating models' >&2
 Rscript lm.R
+echo '-> generating result plots' >&2
 Rscript plot.R
+echo '-> generating count bedgraphs' >&2
 Rscript bedgraph.R

5aa7040c0fde4a31a33badd8327a838ff9943070
qwx <konstantinn.bonnet@etu.univ-lyon1.fr>
2020-08-10 15:04:54 +0200

    count.sh: add h3k9me3 counts

--- count.sh
+++ count.sh
@@ -11,6 +11,9 @@ cp huvec/h3k4me3.bed.gz prep/huvec.h3k4me3.bed.gz
 cp huvec/h3k27ac.bed.gz prep/huvec.h3k27ac.bed.gz
 cp huvec/h3k27me3.bed.gz prep/huvec.h3k27me3.bed.gz
 cp huvec/dhs.rep1.bed.gz prep/huvec.dhs.rep1.bed.gz
+zcat huvec/h3k9me3.bedgraph.gz |\
+	grep -v '^\(track\|chrM\)' |\
+	gzip -c >prep/huvec.h3k9me3.bed.gz
 
 bedtools merge -s -c 4,5,6 -o distinct,mean,distinct -i prep/huvec.groseq.allrep.bed.gz |\
 	sort -k1V,1 -k2n,2 |\
@@ -26,6 +29,14 @@ bedtools map -c 5 -o sum -null 0 -a prep/hg19w.bed -b prep/huvec.groseq.sum.bed.
 	gzip -c >cnt/huvec.groseq.sumofsum.bed.gz
 bedtools merge -s -d 1 -c 4,5,6 -o distinct,distinct,distinct -i prep/huvec.groseq.sum.bed.gz |\
 	gzip -c >prep/huvec.groseq.merged.bed.gz
+
+bedtools map -c 4 -o mean -null 0 -a prep/hg19w.bed -b prep/huvec.h3k9me3.bed.gz |\
+	gzip -c >cnt/huvec.h3k9me3.mean.bed.gz
+bedtools map -c 4 -o sum -null 0 -a prep/hg19w.bed -b prep/huvec.h3k9me3.bed.gz |\
+	gzip -c >cnt/huvec.h3k9me3.sum.bed.gz
+bedtools coverage -a prep/ab.bed -b prep/huvec.h3k9me3.bed.gz |\
+	awk '{print $1 "\t" $2 "\t" $3 "\t" $11}' |\
+	gzip -c >cnt/huvec.h3k9me3.coverage.bed.gz
 
 zcat prep/huvec.chromhmm.1.active_promoter.bed.gz prep/huvec.chromhmm.2.weak_promoter.bed.gz |\
 	sort -k1V,1 -k2n,2 |\
@@ -52,7 +63,7 @@ zcat prep/huvec.chromhmm.6.weak_enhancer.bed.gz prep/huvec.chromhmm.7.weak_enhan
 # count all elements (proa/prob/repseq) per 100kb window along hg19
 cd prep
 ls -1 *.gz repseq/*.gz |\
-	grep -v 'hg19\.gc\|groseq\.\(allrep\.\|mean\.\|sum\.\)' |\
+	grep -v 'hg19\.gc\|h3k9me3\|groseq\.\(allrep\.\|mean\.\|sum\.\)' |\
 	xargs -I {} -P 8 bash -c \
 		'bedtools coverage -counts -a hg19w.bed -b {} | gzip -c > ../cnt/{}'
 cp hg19.gc.bed.gz ../cnt/hg19.gc.bed.gz
